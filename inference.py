d520367e19fc08b1fd92b3a43ff1db55c50c476d
zzudongxiang <zzudongxiang@163.com>
2025-02-21 20:05:41 +0800

    给权重加载过程添加进度显示

--- inference.py
+++ inference.py
@@ -2,16 +2,18 @@
 
 import os
 import json
+import time
 import torch
 import importlib
+import threading
 from datetime import datetime
 import torch.distributed as dist
-from utils.logger import log_rank0
 from argparse import ArgumentParser
 from transformers import AutoTokenizer
 from safetensors.torch import load_model
 from utils.generate import batch_generate
 from model.deepseek.args import ModelArgs
+from utils.logger import log_rank0, format_time
 
 try:
     import torch_npu
@@ -20,6 +22,32 @@ try:
 except:
     default_device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+def load_model_weight(model, ckpt_path):
+    t0 = datetime.now()
+    stop_event = threading.Event()
+    ckpt_file_path = os.path.join(ckpt_path, f"model{rank}-mp{world_size}.safetensors")
+    weight_size = os.path.getsize(ckpt_file_path)
+    base_capacity = torch.cuda.memory_cached()
+    def print_progress(stop_event, base_capacity, weight_size):
+        now = datetime.now()
+        while not stop_event.is_set():
+            if (datetime.now() - now).total_seconds() > 30:
+                now = datetime.now()
+                elapsed = (datetime.now() - t0).total_seconds()
+                hbm_capacity = torch.cuda.memory_cached() - base_capacity
+                progress = hbm_capacity / weight_size
+                progress = progress if progress < 1 else 1
+                log_rank0(f"Load Weight Progress: {(progress * 100):.2f}% | Elapsed: {format_time(elapsed)}")
+            stop_event.wait(1)
+    thread = threading.Thread(target=print_progress, args=(stop_event, base_capacity, weight_size,))
+    thread.daemon = True
+    thread.start()
+    load_model(model, ckpt_file_path)
+    stop_event.set()
+    thread.join()
+    elapsed = (datetime.now() - t0).total_seconds() / 60.0
+    log_rank0(f"Load Weight Progress: 100% | Elapsed: {format_time(elapsed)}")
 
 def main(ckpt_path: str, config: str, model_args: list, model_name: str, startup_type: str = "online") -> None:
     torch.set_default_dtype(torch.bfloat16)
@@ -55,18 +83,15 @@ def main(ckpt_path: str, config: str, model_args: list, model_name: str, startup
     log_rank0(args)
 
     # 模型预热
-    now = datetime.now()
+    t0 = datetime.now()
     tokenizer = AutoTokenizer.from_pretrained(ckpt_path)
     tokenizer.decode(batch_generate(model, [tokenizer.encode("DeepSeek")], tokenizer.eos_token_id, warmup=True)[0])
-    dur = (datetime.now() - now).total_seconds()
-    log_rank0(datetime.now(), f"Prepare DeepSeek Model in {dur:.2f} sec")
+    dur = (datetime.now() - t0).total_seconds()
+    log_rank0(f"Prepare DeepSeek Model in {dur:.2f} sec")
 
     # 加载模型权重
     if not args.use_random_weights:
-        now = datetime.now()
-        load_model(model, os.path.join(ckpt_path, f"model{rank}-mp{world_size}.safetensors"))
-        dur = (datetime.now() - now).total_seconds() / 60.0
-        log_rank0(f"Load DeepSeek Weight in {dur:.2f} min")
+        load_model_weight(model, ckpt_path)
 
     # 按照启动类型启动对应的实例
     if startup_type == "online":
