e67de7b15e588970acfa5258c2307b1c1a17564f
zzudongxiang <zzudongxiang@163.com>
2025-02-23 02:42:22 +0800

    优化输出日志

--- utils/logger.py
+++ utils/logger.py
@@ -2,11 +2,22 @@ import os
 from datetime import datetime
 
 rank = int(os.getenv("RANK", "0"))
-local_rank = int(os.getenv("LOCAL_RANK", "0"))
-world_size = int(os.getenv("WORLD_SIZE", "1"))
+log_filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+try:
+    if not os.path.exists("log"):
+        os.makedirs("log", exist_ok=True)
+    log_file = open(f"log/{log_filename}.log", "w+", encoding="utf8")
+except:
+    log_file = None
+    print(f"Unable to write to log file: {log_filename}")
 
 def log(message):
-    print(f"[{datetime.now()}] {message}")
+    log_str = f"[{datetime.now()}] {message}\n"
+    if log_file is not None:
+        log_file.writelines(log_str)
+        log_file.flush()
+    print(log_str[:-1])
 
 def log_rank0(message):
     if rank != 0:
--- utils/progress.py
+++ utils/progress.py
@@ -1,7 +1,10 @@
+import os
 import threading
 from datetime import datetime
 from dataclasses import dataclass
 from utils.logger import log_rank0, format_time
+
+rank = int(os.getenv("RANK", "0"))
 
 @dataclass
 class ThreadTokens:
@@ -9,7 +12,8 @@ class ThreadTokens:
     stop_event: threading.Event
 
 def print_progress(stop_event, read_progress_func, description="", interval=10):
-    t0 = now = datetime.now()
+    t0 = datetime.now()
+    now = datetime(1970, 1, 1)
     while not stop_event.is_set():
         if (datetime.now() - now).total_seconds() > interval:
             now = datetime.now()
@@ -17,17 +21,21 @@ def print_progress(stop_event, read_progress_func, description="", interval=10):
             progress = read_progress_func()
             progress = progress if progress < 1 else 1
             if progress > 0:
+                eta = (elapsed / progress) * (1 - progress)
+                eta = eta if eta < 360060 else 360060
                 log_rank0(f"{description}: {(progress * 100):05.02f}% | "
                             f"Elapsed: {format_time(elapsed)} | "
-                            f"ETA: {format_time((elapsed / progress) * (1 - progress))}")
+                            f"ETA: {format_time(eta)}")
             else:
-                log_rank0(f"{description}: --.--% | "
+                log_rank0(f"{description}: 0.000% | "
                             f"Elapsed: {format_time(elapsed)}")
         stop_event.wait(1)
     total_time = (datetime.now() - t0).total_seconds()
     log_rank0(f"{description}: 100.0% | Total time: {format_time(total_time)}")
 
 def start_progress(read_progress_func, reset_progress_value=None, description="", interval=10):
+    if rank != 0:
+        return None
     if reset_progress_value is not None:
         reset_progress_value()
     stop_event = threading.Event()
@@ -37,5 +45,7 @@ def start_progress(read_progress_func, reset_progress_value=None, description=""
     return ThreadTokens(thread, stop_event)
 
 def stop_progress(thread_tokens):
+    if rank != 0:
+        return
     thread_tokens.stop_event.set()
     thread_tokens.thread.join()
